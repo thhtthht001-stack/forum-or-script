18ef62b3781b23f11513c611d6bb571a01d6a291
thhtthht001-stack <thhtthht001@gmail.com>
2026-07-27 23:32:15 +0300

    Create script for forum.js

---
+++ script for forum.js
@@ -0,0 +1,60 @@
+{
+  "scripts": [
+    {
+      "id": "button",
+      "name": "Кнопки переходники",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/button",
+      "enabledDefault": true
+    },
+    {
+      "id": "button-old",
+      "name": "Кнопки переходники (OLD)",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/button-old",
+      "enabledDefault": true
+    },
+    {
+      "id": "perenos",
+      "name": "Переносчик тем",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/perenos.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "snow-forum",
+      "name": "Зимняя тема",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/snow-forum.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "timers",
+      "name": "Таймеры",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/timers.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "custom-forum",
+      "name": "Кастомизация",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/custom-forum.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "forum-tech",
+      "name": "Ответы для специалистов",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "",
+      "enabledDefault": true
+    },
+    {
+      "id": "forum-kts",
+      "name": "Ответы для КТС/ЗКТС",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/forum-kts",
+      "enabledDefault": true
+    }
+  ]
+}
