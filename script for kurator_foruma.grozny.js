c95c6082a44cd0725aba2375fbfb0d27c68aeb1d
thhtthht001-stack <thhtthht001@gmail.com>
2026-07-28 00:10:06 +0300

    Update script for kurator_foruma.grozny.js

--- script for kurator_foruma.grozny.js
+++ script for kurator_foruma.grozny.js
@@ -1,1 +1,46 @@
-
+{
+  "scripts": [
+    {
+      "id": "button",
+      "name": "Кнопки переходники",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/button",
+      "enabledDefault": true
+    },
+    {
+      "id": "perenos",
+      "name": "Переносчик тем",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/perenos.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "snow-forum",
+      "name": "Зимняя тема",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/snow-forum.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "timers",
+      "name": "Таймеры",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/timers.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "custom-forum",
+      "name": "Кастомизация",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://gitlab.com/kumiholegenda/script-for-forum/-/raw/main/custom-forum.js",
+      "enabledDefault": true
+    },
+    {
+      "id": "forum-kurator",
+      "name": "Ответы для Кураторов форума",
+      "matches": ["https://forum.blackrussia.online/*"],
+      "url": "https://raw.githubusercontent.com/thhtthht001-stack/forum-or-script/refs/heads/main/tech.js",
+      "enabledDefault": true
+    }
+  ]
+}
